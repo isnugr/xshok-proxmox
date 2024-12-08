ef273d02694448969a35673ef9a5e6b2ef714869
isnugr <96040764+isnugr@users.noreply.github.com>
2024-12-08 14:14:49 +0700

    Update install-post.sh

--- install-post.sh
+++ install-post.sh
@@ -263,10 +263,8 @@ fi
 cat <<EOF > /etc/apt/sources.list
 deb https://mr.heru.id/debian ${OS_CODENAME} main contrib
 deb https://mr.heru.id/debian ${OS_CODENAME}-updates main contrib
-# non-free
-deb https://mr.heru.id/debian/ ${OS_CODENAME} main contrib non-free
 # security updates
-deb https://security.debian.org/debian-security ${OS_CODENAME}/updates main contrib
+deb http://security.debian.org ${OS_CODENAME}-security main contrib
 EOF
 
 # Refresh the package lists
